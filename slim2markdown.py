ee1157698ecf81fef7cbcfec22836c87b815b07b
Junninho Thomas <junninhothomas1998@gmail.com>
2017-07-27 11:14:30 -0400

    added glossary and companies

--- slim2markdown.py
+++ slim2markdown.py
@@ -24,39 +24,41 @@ for url in urls:
 
             for link in soup.find_all('a'):
                 links = link.get('href')
-                check = re.compile("qa|define|learn|guide")
+                check1 = re.compile("life-insurace|health-insurance|renters-insurance|pet-insurance|long-term-disability-insurance")
+                check2 = re.compile("qa|define|learn|guide|glossary|companies")
                 if links is not None:
-                    if check.search(links):
-                        if links.startswith('/'):
-                            links = "https://www.policygenius.com" + links
-                            page = requests.get(links)
-                            page = h.handle(page.text)
-                            links = links.replace('https://www.policygenius.com', '')
-                            links = links[1:]
-                            if links.endswith('/'):
-                                links = links[:-1]
-                            links = links.replace('/', '_')
-                            print(links)
-                            if links.startswith('life-insurance'):
-                                f=open('life-insurance/' + links + '.md', 'w')
-                                f.write(page)
-                                f.close()
-                            elif links.startswith('health-insurance'):
-                                f=open('health-insurance/' + links + '.md', 'w')
-                                f.write(page)
-                                f.close()
-                            elif links.startswith('pet-insurance'):
-                                f=open('pet-insurance/' + links + '.md', 'w')
-                                f.write(page)
-                                f.close()
-                            elif links.startswith('renters-insurance'):
-                                f=open('renters-insurance/' + links + '.md', 'w')
-                                f.write(page)
-                                f.close()
-                            elif links.startswith('long-term-disability-insurance'):
-                                f=open('long-term-disability-insurance/' + links + '.md', 'w')
-                                f.write(page)
-                                f.close()
+                    if check1.search(links):
+                        if check2.search(links):
+                            if links.startswith('/'):
+                                links = "https://www.policygenius.com" + links
+                                page = requests.get(links)
+                                page = h.handle(page.text)
+                                links = links.replace('https://www.policygenius.com', '')
+                                links = links[1:]
+                                if links.endswith('/'):
+                                    links = links[:-1]
+                                links = links.replace('/', '_')
+                                print(links)
+                                if links.startswith('life-insurance'):
+                                    f=open('life-insurance/' + links + '.md', 'w')
+                                    f.write(page)
+                                    f.close()
+                                elif links.startswith('health-insurance'):
+                                    f=open('health-insurance/' + links + '.md', 'w')
+                                    f.write(page)
+                                    f.close()
+                                elif links.startswith('pet-insurance'):
+                                    f=open('pet-insurance/' + links + '.md', 'w')
+                                    f.write(page)
+                                    f.close()
+                                elif links.startswith('renters-insurance'):
+                                    f=open('renters-insurance/' + links + '.md', 'w')
+                                    f.write(page)
+                                    f.close()
+                                elif links.startswith('long-term-disability-insurance'):
+                                    f=open('long-term-disability-insurance/' + links + '.md', 'w')
+                                    f.write(page)
+                                    f.close()
 
 
 # e = 'echo `slimrb -c thanks.html.slim`'
